63b29fc1c4f48cfa750c3a9d9a6b3940d5573e09
Johnathan Williams <jwill387@vols.utk.edu>
2022-10-26 12:12:27 -0400

    Deal hands, attempt to convert face cards to values

--- server.R
+++ server.R
@@ -2,7 +2,8 @@ library(shiny)
 
 shinyServer(function(input, output) {
   observeEvent(input$deal, {
-
+    userHand <- sample(deck, size = 2, replace = FALSE)
+    dealerHand <- sample(deck, size = 2, replace = FALSE)
   })
 
 })
