ab727c0f841d900f38c7ee12ed866bb0e5f886fc
Johnathan Williams <jwill387@vols.utk.edu>
2022-10-29 12:44:00 -0400

    Hacktober PR

--- scratch.R
+++ scratch.R
@@ -2,8 +2,9 @@ userHand <- sample(deck, size = 2, replace = FALSE)
 userHand <- c('KD', '9S')
 
 userVals <- substr(userHand, 1, nchar(userHand) - 1)
+userVals <- as.factor(userVals)
 
 
-if (any(userVals) %in% c("J","Q","K")) {
+if (userVals %in% c("J","Q","K")) {
   userHand[userVals %in% c("J","Q","K")] <- paste0(10, substr(userHand, nchar(userHand), nchar(userHand)))
 }
--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(shiny)
 shinyServer(function(input, output) {
   observeEvent(input$deal, {
     userHand <- sample(deck, size = 2, replace = FALSE)
-    dealerHand <- sample(deck, size = 2, replace = FALSE)
+
   })
 
 })
