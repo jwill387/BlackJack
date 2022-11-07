fa870d9481d9d753a07711fcbcc1e122f82c30da
Adam Spannbauer <SpannbauerAdam@gmail.com>
2022-11-07 09:34:14 -0500

    simplify server observers

--- server.R
+++ server.R
@@ -1,14 +1,11 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$new_deal <- observeEvent(input$deal, {
-    my_reactive_value <- eventReactive(input$button, {
-      GAMESTATE <- initialize_game()
-    })
-
-    output$TEST <- renderPrint({
-      GAMESTATE
-    })
+function(input, output) {
+  GAMESTATE <- eventReactive(input$deal, {
+    initialize_game()
   })
 
-})
+  output$TEST <- renderPrint({
+    GAMESTATE()
+  })
+}
